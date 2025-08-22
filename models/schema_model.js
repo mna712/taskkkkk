ae6ba92b3a1bf0c489aad20f028acd5555c46e9d
mna712 <mennaelgharabawii@gmail.com>
2025-08-22 19:09:39 +0300

    nm

--- models/schema_model.js
+++ models/schema_model.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
+const { type } = require("os");
+const Schema = mongoose.Schema;
+const questionSchema = new Schema({
+  questionTitle: { type: String, required: true },
+  questionDescription: { type: String, required: true },
+  questionSubject: { type: String, required: true },
+});
 
+const answerSchema = new Schema({
+  answerText: { type: String, required: true },
+  questionId: { type: String, required: true },
+});
+
+module.exports = {
+  questionSchema,
+  answerSchema,
+};
