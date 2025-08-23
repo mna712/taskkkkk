1bd70ed054742e93dc151b08598579fd170fb995
Hadeer Mostafa <Hadeer.Mostafa00@outlook.com>
2025-08-24 00:42:28 +0300

    add routes

---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { getQuestions, postQuestion, getQuestionById, postAnswer} = require('../controllers/questionAndA_C');
+
+router.get("/quest",getQuestions);
+router.post("/quest",postQuestion);
+router.get("/quest/:id",getQuestionById);
+router.post("/answer/:id",postAnswer);
+
+module.exports = router;
