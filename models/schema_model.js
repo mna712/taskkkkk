917cc58dc2854bbee93a03f1ecb934a90556eac2
Menna Elgharabawii <162707844+mna712@users.noreply.github.com>
2025-08-24 02:56:39 +0300

    Update schema_model.js
    Signed-off-by: Menna Elgharabawii <162707844+mna712@users.noreply.github.com>

--- models/schema_model.js
+++ models/schema_model.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
+const { type } = require("os");
+const Schema = mongoose.Schema;
+const questionSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  subject: { type: String, required: true },
+});
 
+const answerSchema = new Schema({
+  text: { type: String, required: true },
+  questionId: { type: String, required: true },
+});
+
+module.exports = {
+  questionSchema,
+  answerSchema,
+};
